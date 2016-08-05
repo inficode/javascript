1c5f76e999488e75f193afd25c8b086e799bc0ae
Karthikeyan A K <karthikeyan.ak@zanec.com>
2016-08-05 12:07:55 +0530

    friends done

--- friends/friends.js
+++ friends/friends.js
@@ -3,7 +3,15 @@ $(document).ready(function(){
 
   $("#save-friend").click(function(event){
     var friendName = $("#friend-name").val();
-    $("#friends-list").append("<li>"+friendName+"&nbsp;&nbsp;<span>X</span></li>");
+    $("#friends-list").append("<li>"+friendName+"&nbsp;&nbsp;<span class='remove'>X</span></li>");
+    $(".remove:last").bind("click", function(){
+      $(this).parent().remove();
+    });
     localStorage.friends_list = $("#friends-list").html();
-  })
+  });
+
+  $(".remove").click(function(event){
+    $(this).parent().remove();
+    localStorage.friends_list = $("#friends-list").html();
+  });
 });
