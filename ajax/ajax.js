d40f68fc26a1346965b78b1b97745fedc2e8e6cd
Karthikeyan A K <karthikeyan.ak@zanec.com>
2016-08-09 11:19:53 +0530

    did some changes

--- ajax/ajax.js
+++ ajax/ajax.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
   $("#fetch").click(function(event){
     $("#story").load("http://localhost:8000/story.txt");
+    // $("#story").load("file:///home/ak/code/javascript/ajax/story.txt");
   });
 });
